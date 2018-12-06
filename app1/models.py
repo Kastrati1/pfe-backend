eaffae0eb3bc2308f1841968086d60cc8d916721
klevis <klevis.xhakollari@student.vinci.be>
2018-12-06 12:42:09 +0100

    models changed

--- app1/models.py
+++ app1/models.py
@@ -6,7 +6,6 @@ class Categorie(models.Model):
     name = models.CharField(max_length=50)
 
 class Product(models.Model):
-<<<<<<< HEAD
     name = models.CharField(max_length=50)
     price = models.FloatField()
     description = models.CharField(max_length=100)
@@ -26,22 +25,7 @@ class Commande(models.Model):
     user_id = models.ForeignKey(User,on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.IntegerField()
-
-
-
-
-
-
-
-=======
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=40)
 
-class User(models.Model):
-    #TEMPLATE
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    login = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
->>>>>>> 581adaf1069d02cee2b8fe4d4a7c37186ada86d6
+
