4f84563ddd67b27fbf862f463487fe905bcc6f85
Kandahar <said.imran@student.vinci.be>
2018-12-06 22:08:30 +0100

    first testcase

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTests(APITestCase):
+    def test_create_User(self):
+        """
+        Ensure we can create a new User object.
+        """
+        # url = reverse('inscription')
+        data = {'name': 'DabApps'}
+        response = self.client.post('/inscription/', data, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_404_NOT_FOUND)
+        # self.assertEqual(User.objects.count(), 1)
+        # self.assertEqual(User.objects.get().name, 'DabApps')
