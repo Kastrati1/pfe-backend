de12a28acee804da248de36c160a7a23a2f4cb34
Kandahar <said.imran@student.vinci.be>
2018-12-06 17:28:27 +0100

    backend bug fixes

--- app1/models.py
+++ app1/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -9,21 +10,24 @@ class User(models.Model):
     login = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
+
 class Categorie(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    price = models.FloatField()
+    price = models.FloatField(default=0)
     description = models.CharField(max_length=100)
-    stock = models.IntegerField()
-    categorie_id = models.ForeignKey(Categorie,on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    categorie_id = models.ForeignKey(
+        Categorie, on_delete=models.CASCADE, default=1)
+
 
 class Command(models.Model):
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    product_id = models.ForeignKey(
+        Product, on_delete=models.CASCADE, default=1)
+    quantity = models.IntegerField(default=0)
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=40)
-
-
