a91de1540768e3f929d4463e118f44602a5433b3
klevis <klevis.xhakollari@student.vinci.be>
2018-12-06 12:27:48 +0100

    models added

--- app1/models.py
+++ app1/models.py
@@ -2,7 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+class Categorie(models.Model):
+    name = models.CharField(max_length=50)
 
 class Product(models.Model):
-    name = models.CharField(max_length=10)
-    description = models.CharField(max_length=40)
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    categorie_id = models.ForeignKey(Categorie,on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+class Commande(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+
+
+
+
+
