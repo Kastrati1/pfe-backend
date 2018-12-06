f66d812e16fe923564a70469317ca58c10ba1fe3
Kandahar <said.imran@student.vinci.be>
2018-12-06 20:31:40 +0100

    adding Procfile

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,4 @@ REST_FRAMEWORK = {
     )
 }
 CORS_ORIGIN_ALLOW_ALL = True
+django_heroku.settings(locals())
