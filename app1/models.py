9a4325b55138302e18d3286cf628b80332050cda
sebastien <sebastien.pauwels@student.vinci.be>
2018-12-06 11:33:23 +0100

    Route inscription

--- app1/models.py
+++ app1/models.py
@@ -6,3 +6,11 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=40)
+
+class User(models.Model):
+    #TEMPLATE
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
--- app1/serializers.py
+++ app1/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, User
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('name', 'description')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 
+        'last_name',
+        'email',
+        'login',
+        'password',)
