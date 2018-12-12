88a67229d689dd4d63019f1df73ec91c395b92e6
root <root@MSI.localdomain>
2018-12-12 13:59:37 +0100

    trying dotenv with heroku

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
+import os
+
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
 
 def index(request):
-    html = "<html><body><h1> API UP AND RUNNING - PFE </h1></body></html>"
+    html = "<html><body><h1> API UP AND RUNNING - PFE" + os.environ['ENV'] + " </h1></body></html>"
     return HttpResponse(html)
